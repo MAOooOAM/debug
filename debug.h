cfa77fe871ff2eda8f0e5bb4ff80a81edf5fb6c1
MAOooOAM <lidingmao0205@163.com>
2023-05-18 21:07:57 +0800

    Update ``debug.h``
    1. Rename namespace ``debug`` to ``dbg``
    2. Add marcos

--- debug.h
+++ debug.h
@@ -10,53 +10,79 @@
 #include <vector>
 
 
-namespace debug {
+#define __UNPACK_IMPL__(...) __VA_ARGS__
+#define __UNPACK__(PACK) __UNPACK_IMPL__ PACK
 
-std::ostream* debug_stream_ptr = &std::cerr;
-void set_debug_stream(std::ostream& os) { debug_stream_ptr = &os; }
+#define __ARG1__(_1, ...) _1
+#define __ARG2__(_1, _2, ...) _2
+#define __ARG3__(_1, _2, _3, ...) _3
+#define __ARG4__(_1, _2, _3, _4, ...) _4
+#define __ARG64__(_1, _2, _3, _4, _5, _6, _7, _8, _9, _10, _11, _12, _13, _14, _15, _16, _17, _18, \
+                  _19, _20, _21, _22, _23, _24, _25, _26, _27, _28, _29, _30, _31, _32, _33, _34,  \
+                  _35, _36, _37, _38, _39, _40, _41, _42, _43, _44, _45, _46, _47, _48, _49, _50,  \
+                  _51, _52, _53, _54, _55, _56, _57, _58, _59, _60, _61, _62, _63, _64, ...)       \
+  _64
 
-}  // namespace debug
+#define __RESQ64__                                                                                \
+  63, 62, 61, 60, 59, 58, 57, 56, 55, 54, 53, 52, 51, 50, 49, 48, 47, 46, 45, 44, 43, 42, 41, 40, \
+      39, 38, 37, 36, 35, 34, 33, 32, 31, 30, 29, 28, 27, 26, 25, 24, 23, 22, 21, 20, 19, 18, 17, \
+      16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0
 
-using debug::set_debug_stream;
+#define __NARG_IMPL__(...) __ARG64__(__VA_ARGS__)
+#define __NARG__(...) __NARG_IMPL__(__VA_ARGS__, __RESQ64__)
+
+#define FOO_NAME_ARY(NAME, N) NAME##_##N##ARY
+#define FOO_NAME(NAME, N) FOO_NAME_ARY(NAME, N)
+/** Use
+ *  #define FOO(...) FOO_NAME(FOO, __NARG__(__VA_ARGS__))(__VA_ARGS__)
+ *  to define variable number of arguments.
+ */
+
+
+#define DECLARE_V_HELPER(...) FOO_NAME(DECLARE_V_HELPER, __NARG__(__VA_ARGS__))(__VA_ARGS__)
+#define DECLARE_V_HELPER_1ARY(NAME) DECLARE_V_HELPER_3ARY(NAME, (typename T), (T))
+#define DECLARE_V_HELPER_3ARY(NAME, PACK1, PACK2) \
+  template <__UNPACK__(PACK1)>                    \
+  constexpr typename NAME<__UNPACK__(PACK2)>::value_type NAME##_v = NAME<__UNPACK__(PACK2)>::value
 
 
 template <typename T, typename... Ts>
 struct is_one_of : std::disjunction<std::is_same<T, Ts>...> {};
-template <typename T, typename... Ts> constexpr bool is_one_of_v = is_one_of<T, Ts...>::value;
+DECLARE_V_HELPER(is_one_of, (typename T, typename... Ts), (T, Ts...));
+
 
 template <typename T, template <typename...> class Template>
 struct is_specialization_of : std::false_type {};
 template <template <typename...> class Template, typename... Ts>
 struct is_specialization_of<Template<Ts...>, Template> : std::true_type {};
-template <typename T, template <typename...> class Template>
-constexpr bool is_specialization_of_v = is_specialization_of<T, Template>::value;
+DECLARE_V_HELPER(is_specialization_of, (typename T, template <typename...> class Template),
+                 (T, Template));
 
-#define DECLARE_IS_SPEC_1(NAME)                                              \
-  template <typename T> struct is_##NAME : is_specialization_of<T, NAME> {}; \
-  template <typename T> constexpr bool is_##NAME##_v = is_##NAME<T>::value;
-#define DECLARE_IS_SPEC_2(FIELD, NAME)                                              \
-  template <typename T> struct is_##NAME : is_specialization_of<T, FIELD::NAME> {}; \
-  template <typename T> constexpr bool is_##NAME##_v = is_##NAME<T>::value;
-#define GET3(ARG1, ARG2, ARG3, ...) ARG3
-#define DECLARE_IS_SPEC_IMPL(...) \
-  GET3(__VA_ARGS__, DECLARE_IS_SPEC_2, DECLARE_IS_SPEC_1, )(__VA_ARGS__)
-#define DECLARE_IS_SPEC(...) DECLARE_IS_SPEC_IMPL(__VA_ARGS__)
 
-DECLARE_IS_SPEC(std, pair)
-DECLARE_IS_SPEC(std, tuple)
-DECLARE_IS_SPEC(std, queue)
-DECLARE_IS_SPEC(std, priority_queue)
+#define DECLARE_IS_SPEC(...) FOO_NAME(DECLARE_IS_SPEC, __NARG__(__VA_ARGS__))(__VA_ARGS__)
+#define DECLARE_IS_SPEC_1ARY(NAME) DECLARE_SPEC_TRAITS(is_##NAME, NAME)
+#define DECLARE_IS_SPEC_2ARY(FIELD, NAME) DECLARE_SPEC_TRAITS(is_##NAME, FIELD::NAME)
+#define DECLARE_SPEC_TRAITS(SPEC, TEMP)                                 \
+  template <typename T> struct SPEC : is_specialization_of<T, TEMP> {}; \
+  DECLARE_V_HELPER(SPEC)
+
+DECLARE_IS_SPEC(std, pair);
+DECLARE_IS_SPEC(std, tuple);
+DECLARE_IS_SPEC(std, queue);
+DECLARE_IS_SPEC(std, priority_queue);
+
 
 template <typename T> using begin_type = decltype(std::begin(std::declval<T>()));
 template <typename, typename = void> struct has_begin_helper : std::false_type {};
 template <typename T> struct has_begin_helper<T, std::void_t<begin_type<T>>> : std::true_type {};
 template <typename T> using has_begin = has_begin_helper<T>;
-template <typename T> constexpr bool has_begin_v = has_begin<T>::value;
+DECLARE_V_HELPER(has_begin);
+
 
 template <typename T>
 struct is_rai : std::is_same<typename std::iterator_traits<T>::iterator_category,
                              std::random_access_iterator_tag> {};
-template <typename T> constexpr bool is_rai_v = is_rai<T>::value;
+DECLARE_V_HELPER(is_rai);
 
 
 constexpr std::string_view c_escape(const char& c) {
@@ -144,6 +170,7 @@ template <typename T> void print_impl(std::ostream& os, T&& x) {
     os << x;
   }
 }
+
 template <typename Tuple, std::size_t... Is>
 void print_tuple_impl(std::ostream& os, Tuple&& t, std::index_sequence<Is...>) {
   (print_impl(os << (Is ? ',' : '<'), std::get<Is>(std::forward<Tuple>(t))), ...);
@@ -151,9 +178,39 @@ void print_tuple_impl(std::ostream& os, Tuple&& t, std::index_sequence<Is...>) {
 }
 
 
+namespace dbg {
+
+std::ostream* debug_stream_ptr = &std::cerr;
+void set_debug_stream(std::ostream& os) { debug_stream_ptr = &os; }
+
+}  // namespace dbg
+
+using dbg::set_debug_stream;
+
 template <typename... Ts> void debug_impl(Ts&&... args) {
-  (..., print_impl(*debug::debug_stream_ptr << ' ', std::forward<Ts>(args)));
-  *debug::debug_stream_ptr << std::endl;
+  (..., print_impl(*dbg::debug_stream_ptr << ' ', std::forward<Ts>(args)));
+  *dbg::debug_stream_ptr << std::endl;
 }
 
-#define debug(...) *debug::debug_stream_ptr << "[" #__VA_ARGS__ "] :", debug_impl(__VA_ARGS__)
+#define debug(...) *dbg::debug_stream_ptr << "[" #__VA_ARGS__ "] :", debug_impl(__VA_ARGS__)
+
+
+#undef __UNPACK_IMPL__
+#undef __UNPACK__
+#undef __ARG1__
+#undef __ARG2__
+#undef __ARG3__
+#undef __ARG4__
+#undef __ARG64__
+#undef __RESQ64__
+#undef __NARG_IMPL__
+#undef __NARG__
+#undef FOO_NAME_ARY
+#undef FOO_NAME
+#undef DECLARE_V_HELPER
+#undef DECLARE_V_HELPER_1ARY
+#undef DECLARE_V_HELPER_3ARY
+#undef DECLARE_IS_SPEC
+#undef DECLARE_IS_SPEC_1ARY
+#undef DECLARE_IS_SPEC_2ARY
+#undef DECLARE_SPEC_TRAITS
